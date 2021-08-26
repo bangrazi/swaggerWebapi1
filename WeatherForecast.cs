f910e2b986224bcdab7c3d19ca108c2ed4293e0c
Anthony Bangrazi <bangrazi@comcast.net>
2021-08-26 18:24:39 -0400

    fixed schema "required" values

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -3,7 +3,7 @@ using Swashbuckle.AspNetCore.Annotations;
 
 namespace swaggerWebapi1
 {
-    [SwaggerSchema(Required = new [] { "Forecast" })]
+    [SwaggerSchema(Required = new [] { nameof(TemperatureC), nameof(Summary) })]
     public class WeatherForecast
     {
         [SwaggerSchema(Description = "The timestamp of the forecast.")]
@@ -13,7 +13,7 @@ namespace swaggerWebapi1
 
         public int TemperatureC { get; set; }
 
-        [SwaggerSchema(Description = "The temperature in degrees Farenheit.", ReadOnly = true)]
+        [SwaggerSchema(Description = "The temperature in degrees Fahrenheit.", ReadOnly = true)]
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         [SwaggerSchema(Description = "A summary of the forecast.")]
